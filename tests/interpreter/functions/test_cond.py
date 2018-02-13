41123a74e4a788d69a49f46aa140186df708d422
Andrey <andrey.nagornov@hotmail.com>
2018-02-14 00:13:44 +0300

    Добавляет тесты для условных функций

---
+++ tests/interpreter/functions/test_cond.py
@@ -0,0 +1,121 @@
+import pytest
+
+from interpreter.functions.conditional import *
+
+
+class TestIf:
+    """
+    Выбор по логическому условию
+    """
+
+    def test_if1(self):
+        assert _if(1, 100, 200) == 100
+
+    def test_if2(self):
+        assert _if(0, 100, 200) == 200
+
+    def test_typeerror(self):
+        with pytest.raises(TypeError):
+            _if(10, "two")
+
+
+class TestNcase0:
+    """
+    Выбор по индексу от нуля
+    """
+    
+    def test_ncase1(self):
+        assert ncase0(0, 100, 4, 5, 6, 7) == 4
+
+    def test_ncase2(self):
+        assert ncase0(2, 100, 4, 5, 6, 7) == 6
+
+    def test_ncase3(self):
+        assert ncase0(9, 100, 4, 5, 6, 7) == 100
+
+    def test_typeerror(self):
+        with pytest.raises(TypeError):
+            ncase0(10, "two", 4, 5, 6, 7)
+
+
+class TestNcase1:
+    """
+    Выбор по индексу от единицы
+    """
+
+    def test_ncase1(self):
+        assert ncase1(0, 100, 4, 5, 6, 7) == 100
+
+    def test_ncase2(self):
+        assert ncase1(1, 100, 4, 5, 6, 7) == 4
+
+    def test_ncase3(self):
+        assert ncase1(2, 100, 4, 5, 6, 7) == 5
+
+    def test_ncase4(self):
+        assert ncase1(9, 100, 4, 5, 6, 7) == 100
+
+    def test_typeerror(self):
+        with pytest.raises(TypeError):
+            ncase1(10, "two", 4, 5, 6, 7)
+
+
+class TestRcase:
+    """
+    Выбор по диапазонам V(n) >= S < V(n+1)
+    """
+
+    def test_rcase1(self):
+        assert rcase(-20, "Very cold", -10, "Cold", 5, "Norm", 40, "Hot") == "Very cold"
+
+    def test_rcase2(self):
+        assert rcase(-10, "Very cold", -10, "Cold", 5, "Norm", 40, "Hot") == "Cold"
+
+    def test_rcase3(self):
+        assert rcase(7, "Very cold", -10, "Cold", 5, "Norm", 40, "Hot") == "Norm"
+
+    def test_rcase4(self):
+        assert rcase(500, "Very cold", -10, "Cold", 5, "Norm", 40, "Hot") == "Hot"
+
+    def test_typeerror(self):
+        with pytest.raises(TypeError):
+            rcase("500", "Very cold", "-10", "Cold", "5", "Norm", "40", "Hot")
+
+
+class TestLcase:
+    """
+    Выбор по диапазонам V(n) > S =< V(n+1)
+    """
+
+    def test_lcase1(self):
+        assert lcase(-20, "Very cold", -10, "Cold", 5, "Norm", 40, "Hot") == "Very cold"
+
+    def test_lcase2(self):
+        assert lcase(-10, "Very cold", -10, "Cold", 5, "Norm", 40, "Hot") == "Very cold"
+
+    def test_lcase3(self):
+        assert lcase(-11, "Very cold", -10, "Cold", 5, "Norm", 40, "Hot") == "Cold"
+
+    def test_lcase3(self):
+        assert lcase(7, "Very cold", -10, "Cold", 5, "Norm", 40, "Hot") == "Norm"
+
+    def test_lcase4(self):
+        assert lcase(500, "Very cold", -10, "Cold", 5, "Norm", 40, "Hot") == "Hot"
+
+    def test_typeerror(self):
+        with pytest.raises(TypeError):
+            lcase("500", 500, "-10", 5, "5", 40, "40", 500)
+
+
+class TestTcase:
+    """
+    Выбор значения по ключу
+    !!! Необходимо уточнить (могут ли быть строки)
+    !!! уточнить про tcase_v2
+    """
+
+    def test_tcase1(self):
+        assert tcase(2, 1, 100, 2, 200, 3, 300) == 200
+
+    def test_tcase1(self):
+        assert tcase(7, 1, 100, 2, 200, 3, 300) == ''
