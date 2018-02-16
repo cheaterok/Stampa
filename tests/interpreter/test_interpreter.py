d96104e59f7bbed0eb0bc4cc0671d2fed9c945a8
Andrey <andrey.nagornov@hotmail.com>
2018-02-16 18:34:15 +0300

    Добавляет функции тестирования интерпритатора

---
+++ tests/interpreter/test_interpreter.py
@@ -0,0 +1,31 @@
+import pytest
+
+
+def test_0():
+	assert interpreter.evaluate(' add ( r(1.33, 0.25, "up"), neg(1))') == '0.5'
+
+
+def test_1():
+	assert interpreter.evaluate('trim("Hello " "world" )') == 'Hello world'
+
+
+def test_2():
+	assert interpreter.evaluate('nws({Много пробелов это   плохо})  ') == 'Много пробелов это плохо'
+
+
+def test_3():
+	assert interpreter.evaluate('seq  (/Winter Olympics/, <Winter Olympics>)') == '1'
+
+
+def test_4():
+	assert interpreter.evaluate('trim("  "):-Unknown value') == 'Unknown value'
+
+
+def test_5():
+	assert interpreter.evaluate('trim(/  /):+add(2, 2)') == '4'
+
+
+def test_6():
+	assert interpreter.evaluate('trim([" Privet"]):+add(2, 2)') == 'Privet'
+
+
