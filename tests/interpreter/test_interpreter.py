a21d5a7807181c9df122ff9eae9cf38e5d1b88a3
Vsevolod <1cheaterok1@gmail.com>
2018-02-19 16:19:32 +0300

    Исправляет тесты интерпретатора

--- tests/interpreter/test_interpreter.py
+++ tests/interpreter/test_interpreter.py
@@ -1,29 +1,31 @@
 import pytest
 
+import interpreter
+
 
 def test_0():
-	assert interpreter.evaluate(' add ( r(1.33, 0.25, "up"), neg(1))') == '0.5'
+    assert interpreter.evaluate(' add ( r(1.33, 0.25, "up"), neg(1))') == '0.5'
 
 
 def test_1():
-	assert interpreter.evaluate('trim("Hello " "world" )') == 'Hello world'
+    assert interpreter.evaluate('trim("Hello " "world" )') == 'Hello world'
 
 
 def test_2():
-	assert interpreter.evaluate('nws({Много пробелов это   плохо})  ') == 'Много пробелов это плохо'
+    assert interpreter.evaluate('nws({Много пробелов это   плохо})  ') == 'Много пробелов это плохо'
 
 
 def test_3():
-	assert interpreter.evaluate('seq  (/Winter Olympics/, <Winter Olympics>)') == '1'
+    assert interpreter.evaluate('seq  (/Winter Olympics/, <Winter Olympics>)') == '1'
 
 
 def test_4():
-	assert interpreter.evaluate('trim("  "):-Unknown value') == 'Unknown value'
+    assert interpreter.evaluate('trim("  "):-/Unknown value/') == 'Unknown value'
 
 
 def test_5():
-	assert interpreter.evaluate('trim(/  /):+add(2, 2)') == '4'
+    assert interpreter.evaluate('trim(/  /):+add(2, 2)') == '4'
 
 
 def test_6():
-	assert interpreter.evaluate('trim([" Privet"]):+add(2, 2)') == 'Privet'
+    assert interpreter.evaluate('trim([ Privet]):+add(2, 2)') == 'Privet'
